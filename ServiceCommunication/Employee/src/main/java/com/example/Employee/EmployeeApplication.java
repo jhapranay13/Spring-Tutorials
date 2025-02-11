a2d59a3ff6adba004bc72707bd8602fc9339c2ba
jhapranay13 <jhapranay@gmail.com>
2025-02-10 21:04:29 -0700

    updated

--- ServiceCommunication/Employee/src/main/java/com/example/Employee/EmployeeApplication.java
+++ ServiceCommunication/Employee/src/main/java/com/example/Employee/EmployeeApplication.java
@@ -1,8 +1,12 @@
 package com.example.Employee;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.commons.httpclient.HttpClientConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableFeignClients
